8810641767857248f0d7db0e5ad0cff4417e719f
AnnikaChauhan <annikac0711@gmail.com>
2021-02-01 11:32:31 +0000

    adds mui components and trials some functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import React, { useState } from "react";
 import GridLayout from "react-grid-layout";
+import Card from "@material-ui/core/Card";
+import Switch from "@material-ui/core/Switch";
 
 const App = () => {
+  const [draggable, setDraggable] = useState(true);
+
   const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 1, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 2, w: 1, h: 2 },
+    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true, isResizable: true },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
   ];
+
+  const handleChange = (event) => {
+    setDraggable(event.target.checked);
+  };
+
   return (
     <div style={{ margin: "0 10px" }}>
       <h1>React Drag and Drop Dashboard</h1>
+      <div>
+        <p>
+          Draggable:{" "}
+          <Switch
+            checked={draggable}
+            onChange={handleChange}
+            color="primary"
+            name="checkedB"
+            inputProps={{ "aria-label": "primary checkbox" }}
+          />
+        </p>
+      </div>
       <GridLayout
         style={{
           border: "1px solid #e4e4e4",
@@ -20,35 +42,13 @@ const App = () => {
         width={1200}
         margin={[10, 10]}
         containerPadding={[10, 10]}
+        className="layout"
+        isDraggable={draggable}
         // onLayoutChange={() => console.log("layout has changed")}
       >
-        <div
-          key="a"
-          style={{
-            border: "1px solid grey",
-            borderRadius: "6px",
-          }}
-        >
-          Test
-        </div>
-        <div
-          key="b"
-          style={{
-            border: "1px solid grey",
-            borderRadius: "6px",
-          }}
-        >
-          Test
-        </div>
-        <div
-          key="c"
-          style={{
-            border: "1px solid grey",
-            borderRadius: "6px",
-          }}
-        >
-          Test
-        </div>
+        <Card key="a">Card A</Card>
+        <Card key="b">Card B</Card>
+        <Card key="c">Card C</Card>
       </GridLayout>
     </div>
   );
