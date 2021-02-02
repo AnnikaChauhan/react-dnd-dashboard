20cd0ca21f110d918f9a560ff589f522669d7dbe
AnnikaChauhan <annikac0711@gmail.com>
2021-02-02 09:28:53 +0000

    restructures component to get rid of jump

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import {
-  Card,
   Switch,
   Paper,
   Radio,
@@ -9,25 +8,12 @@ import {
   FormControlLabel,
   FormLabel,
 } from "@material-ui/core";
-import useStyles from "./styles";
-import RGL, { WidthProvider } from "react-grid-layout";
-import AspectRatioIcon from "@material-ui/icons/AspectRatio";
-
-const ReactGridLayout = WidthProvider(RGL);
+import DragAndDrop from "./DragAndDrop";
 
 const App = () => {
-  const classes = useStyles();
   const [draggable, setDraggable] = useState(true);
   const [resizable, setResizable] = useState(true);
   const [compactType, setCompactType] = useState("vertical");
-
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
-    { i: "d", x: 1, y: 1, w: 2, h: 3 },
-    { i: "e", x: 2, y: 1, w: 2, h: 2 },
-  ];
 
   const handleDraggableChange = (event) => setDraggable(event.target.checked);
 
@@ -37,7 +23,13 @@ const App = () => {
 
   return (
     <div style={{ margin: "0 10px" }}>
-      <h1>React Drag and Drop Dashboard</h1>
+      <Paper style={{ height: "500px", marginTop: "10px" }}>
+        <DragAndDrop
+          draggable={draggable}
+          resizable={resizable}
+          compactType={compactType}
+        />
+      </Paper>
       <div>
         <p>
           Draggable:{" "}
@@ -87,39 +79,6 @@ const App = () => {
           </FormControl>
         </p>
       </div>
-      <Paper style={{ height: "500px" }}>
-        <ReactGridLayout
-          layout={layout}
-          cols={12}
-          rowHeight={30}
-          width={1200}
-          margin={[10, 10]}
-          containerPadding={[10, 10]}
-          className="layout"
-          isDraggable={draggable}
-          isResizable={resizable}
-          resizeHandles={["se"]}
-          resizeHandle={() => <AspectRatioIcon />}
-          compactType={compactType}
-          // onLayoutChange={() => console.log("layout has changed")}
-        >
-          <Card className={classes.container} key="a">
-            Card A
-          </Card>
-          <Card className={classes.container} key="b">
-            Card B
-          </Card>
-          <Card className={classes.container} key="c">
-            Card C
-          </Card>
-          <Card className={classes.container} key="d">
-            Card D
-          </Card>
-          <Card className={classes.container} key="e">
-            Card E
-          </Card>
-        </ReactGridLayout>
-      </Paper>
     </div>
   );
 };
---
+++ src/DragAndDrop.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Card } from "@material-ui/core";
+import useStyles from "./styles";
+import RGL, { WidthProvider } from "react-grid-layout";
+import AspectRatioIcon from "@material-ui/icons/AspectRatio";
+
+const ReactGridLayout = WidthProvider(RGL);
+
+const DragAndDrop = ({ draggable, resizable, compactType }) => {
+  const classes = useStyles();
+
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 2 },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+    { i: "d", x: 1, y: 1, w: 2, h: 3 },
+    { i: "e", x: 2, y: 1, w: 2, h: 2 },
+  ];
+
+  return (
+    <ReactGridLayout
+      layout={layout}
+      cols={12}
+      rowHeight={30}
+      width={1200}
+      margin={[10, 10]}
+      containerPadding={[10, 10]}
+      className="layout"
+      isDraggable={draggable}
+      isResizable={resizable}
+      resizeHandles={["se"]}
+      resizeHandle={() => <AspectRatioIcon />}
+      compactType={compactType}
+      // onLayoutChange={() => console.log("layout has changed")}
+    >
+      <Card className={classes.container} key="a">
+        Card A
+      </Card>
+      <Card className={classes.container} key="b">
+        Card B
+      </Card>
+      <Card className={classes.container} key="c">
+        Card C
+      </Card>
+      <Card className={classes.container} key="d">
+        Card D
+      </Card>
+      <Card className={classes.container} key="e">
+        Card E
+      </Card>
+    </ReactGridLayout>
+  );
+};
+
+export default DragAndDrop;
