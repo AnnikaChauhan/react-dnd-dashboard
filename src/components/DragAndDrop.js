1f11414c1aeb5e19a1896a6e33a2228f488d0e56
AnnikaChauhan <annikac0711@gmail.com>
2021-02-08 15:09:44 +0000

    amends row height

--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -47,8 +47,8 @@ const DragAndDrop = ({ draggable, resizable, compactType }) => {
       layouts={responsiveLayouts}
       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
       cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-      rowHeight={30}
       width={1200}
+      rowHeight={30}
       margin={[10, 10]}
       containerPadding={[10, 10]}
       className="layout"
